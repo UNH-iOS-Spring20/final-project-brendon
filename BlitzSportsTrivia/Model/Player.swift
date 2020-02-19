b19512f12034533ebeea5e60cc548911f00c35ca
Brendon Malouf <brendonmalouf@gmail.com>
2020-02-18 21:10:20 -0500

    First commit on Tuesday Feb.18. Player Class init, setLevel, baseWinXP

---
+++ BlitzSportsTrivia/Model/Player.swift
@@ -0,0 +1,76 @@
+//
+//  Player.swift
+//  BlitzSportsTrivia
+//
+//  Created by Brendon Malouf on 2/17/20.
+//  Copyright © 2020 Brendon Malouf. All rights reserved.
+//
+
+class Player{
+    //The following constants xp required to level up
+    let maxLevelXP1 = 5000 //5000xp upgrades player to level 2
+    let maxLevelXP2 = 10000 //10000xp upgrades player to level 3
+    let maxLevelXP3 = 30000 //30000xp upgrades player to level 4
+    let maxLevelXP4 = 60000 //60000xp upgrades player to level 5
+    let maxLevelXP5 = 120000 //12000xp is max level of the game
+    let XPArr = [Int](arrayLiteral: 5000, 10000, 30000, 60000, 120000) //Array holding each levels xp values to rank up
+    
+    var displayName:String
+    var totalXP:Int
+    var level:Int
+    var currStreak:Int = 0
+    var gameScore:Int = 0
+    
+//---------------------------------------------------------------
+    //initializes a Player object
+    init?(name: String, totalXP:Int, currStreak:Int) {
+        if name.isEmpty || totalXP < 0 || currStreak < 0 {
+            return nil
+        }
+        self.displayName = name
+        self.totalXP = totalXP
+        self.currStreak = currStreak
+        self.gameScore = 0
+        level = 1
+        setLevel()
+    }
+    
+//---------------------------------------------------------------
+    // setLevel() sets the players level based on totalxp
+    func setLevel(){
+        /*
+         The Levels will be determined by xp in this format:
+         - Level 1: 0 - 4999xp
+         - Level 2: 5000 - 9999xp
+         - Level 3: 10000 - 29999xp
+         - Level 4: 30000 - 59999xp
+         - Level 5: 60000 - 120,000 (Master)
+         */
+        
+        if totalXP < maxLevelXP1{
+            level = 1
+        }
+        else if totalXP >= maxLevelXP1 && totalXP < maxLevelXP2{
+            level = 2
+        }
+        else if totalXP >= maxLevelXP2 && totalXP < maxLevelXP3{
+           level = 3
+        }
+        else if totalXP >= maxLevelXP3 && totalXP < maxLevelXP4{
+            level = 4
+        }
+        else{
+            level = 5
+        }
+    }
+//---------------------------------------------------------------
+    /*
+     baseWinXP() adds a base XP when a player wins to the players totalXP.
+     Amount of base xp is 10% the minimum total xp for the next level.
+     */
+    func baseWinXP(){ //called only if the player wins
+        totalXP = totalXP + (XPArr[level-1] / 10)
+    }
+//---------------------------------------------------------------
+    
+}
--- BlitzSportsTriviaTests/BlitzSportsTriviaTests.swift
+++ BlitzSportsTriviaTests/BlitzSportsTriviaTests.swift
@@ -19,11 +19,44 @@ class BlitzSportsTriviaTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
+    func testPlayerInitSucceeds(){
+        let player1 = Player.init(name: "Brendon", totalXP: 900, currStreak: 0)
+        XCTAssertNotNil(player1)
+        
+        let player2 = Player.init(name: "Lily", totalXP: 4000, currStreak: 3)
+        XCTAssertNotNil(player2)
+    }
+    
+    func testPlayerInitFails(){
+        let player1 = Player.init(name: "", totalXP: 900, currStreak: 0)
+        XCTAssertNil(player1)
+        let player2 = Player.init(name: "Bob", totalXP: -1000, currStreak: 0)
+        XCTAssertNil(player2)
+        let player3 = Player.init(name: "Lily", totalXP: 4000, currStreak: -1)
+        XCTAssertNil(player3)
+    }
+    
+    func testPlayerSetLevel(){
+        let player1 = Player.init(name: "Brendon", totalXP: 0, currStreak: 0) //setLevel is called in init
+        XCTAssertEqual(1, player1?.level)
+        
+        let player2 = Player.init(name: "Bob", totalXP: 4999, currStreak: 0) //setLevel is called in init
+        XCTAssertEqual(1, player2?.level)
+        
+        let player3 = Player.init(name: "Marshall", totalXP: 50000, currStreak: 0) //setLevel is called in init
+        XCTAssertEqual(4, player3?.level)
+        
+        let player4 = Player.init(name: "Marshall", totalXP: 100000, currStreak: 0) //setLevel is called in init
+        XCTAssertEqual(5, player4?.level)
+    }
+    
+    func testPlayerBaseWinXP(){
+        let player1 = Player.init(name: "Bruce", totalXP: 50000, currStreak: 0)
+        player1?.baseWinXP()
+        XCTAssertEqual(56000, player1?.totalXP)//Since Bruce has 50000xp, he is in level 4. Therefore, his base win xp should add 6000 (10% of 60000) to his total xp.
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
